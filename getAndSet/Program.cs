2502d3948ef9fc393368b37d5d13b1c445647f5f
unknown <redaattamahmoud@gmail.com>
2022-09-11 17:24:55 +0200

    setAndGet

---
+++ getAndSet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace getAndSet
+{
+    class person
+    {
+        private string name;
+        public string Name
+        {
+            set { name = value; }
+            get { return name;}
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            person myobJ = new person();
+            myobJ.Name = "Mahmoud Reda";
+            Console.WriteLine(myobJ.Name);
+        }
+    }
+}
